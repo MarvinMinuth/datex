ba395b699d36b8054ee6bf0a094e53927e5c6de7
Marvin Minuth <marvin.minuth@gmail.com>
2025-07-08 15:33:59 +0200

    Refactor extraction and main modules for improved structure and error handling

--- src/datex/extraction.py
+++ src/datex/extraction.py
@@ -1,43 +1,69 @@
 from ollama import AsyncClient
 from openai import AsyncOpenAI
-from datex.models.schemas import ModelConfig 
+from datex.models.schemas import ModelConfig
 from datex.conversion.schemas import InputData, InputType
 
 # TODO: correctly convert text input_data
 
-async def extract_with_ollama(config: ModelConfig, input_data: list[InputData], system_prompt, user_prompt, output_schema):
+
+async def extract_with_ollama(
+    config: ModelConfig,
+    input_data: list[InputData],
+    system_prompt,
+    user_prompt,
+    output_schema,
+):
     ollama_response = await AsyncClient().chat(
         model=config.model_name,
         messages=[
             {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt, "images": [i.input_content for i in input_data if i.input_type == InputType.IMG]}
+            {
+                "role": "user",
+                "content": user_prompt,
+                "images": [
+                    i.input_content for i in input_data if i.input_type == InputType.IMG
+                ],
+            },
         ],
         stream=False,
         format=output_schema,
-        options={"temperature": config.temperature, "top_p": config.top_p}
+        options={"temperature": config.temperature, "top_p": config.top_p},
     )
 
     return ollama_response.message.content
 
+
 def _create_openai_user_prompt(user_prompt: str, input_data: list[InputData]):
     user_content = []
     user_content.append({"type": "input_text", "text": user_prompt})
-    user_content.extend([{
-                        "type": "input_image",
-                        "image_url": f"data:image/png;base64,{i.input_content}"
-                        } for i in input_data if i.input_type == InputType.IMG])
+    user_content.extend(
+        [
+            {
+                "type": "input_image",
+                "image_url": f"data:image/png;base64,{i.input_content}",
+            }
+            for i in input_data
+            if i.input_type == InputType.IMG
+        ]
+    )
     return user_content
 
 
-async def extract_with_openai(config: ModelConfig, input_data: list[InputData], system_prompt, user_prompt, output_schema):
+async def extract_with_openai(
+    config: ModelConfig,
+    input_data: list[InputData],
+    system_prompt,
+    user_prompt,
+    output_schema,
+):
     user_content = _create_openai_user_prompt(user_prompt, input_data)
     client = AsyncOpenAI(api_key=config.api_key)
 
     response = await client.responses.create(
-        model=config.model_name, 
+        model=config.model_name,
         input=[
             {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_content}
+            {"role": "user", "content": user_content},
         ],
         temperature=config.temperature,
         top_p=config.top_p,
@@ -45,8 +71,8 @@ async def extract_with_openai(config: ModelConfig, input_data: list[InputData], 
             "format": {
                 "type": "json_schema",
                 "name": "product_data",
-                "schema": output_schema
+                "schema": output_schema,
             }
-        }
+        },
     )
     return response.output_text
--- src/datex/main.py
+++ src/datex/main.py
@@ -8,70 +8,93 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 import asyncio
-
-# TODO: add expected output
-
-# TODO: move config and prompts to json files
+import argparse
 
 load_dotenv()
 
-async def extract():
-    with open("./config.json", "r") as file:
+parser = argparse.ArgumentParser()
+parser.add_argument("config_path")
+parser.add_argument("output_schema_path")
+parser.add_argument("dataset_path")
+
+
+def load_config(path: Path) -> tuple[ModelConfig, str, str]:
+    with open(path, "r") as file:
         config_file = json.load(file)
 
-    api_key = (
-        config_file.get("api_key")
-        or os.getenv("OPENAI_API_KEY")
-        or ""
-    )
+    api_key = config_file.get("api_key") or os.getenv("OPENAI_API_KEY") or ""
 
     config = ModelConfig(
         provider=config_file["provider"],
         model_name=config_file["model_name"],
         temperature=config_file["temperature"],
         top_p=config_file["top_p"],
-        api_key=api_key
+        api_key=api_key,
     )
 
-    system_prompt = config_file.get("system_prompt", "The Assistant is Datex, an AI assistant to extract data from documents.")
+    system_prompt = config_file.get(
+        "system_prompt",
+        "The Assistant is Datex, an AI assistant to extract data from documents.",
+    )
     user_prompt = config_file.get(
-            "user_prompt", 
-            "Extract the information from this data sheet. If a value is not present, provide null. Dates should be in the format YYYY-MM."
+        "user_prompt",
+        "Extract the information from this data sheet. If a value is not present, provide null. Dates should be in the format YYYY-MM.",
     )
+    return (config, system_prompt, user_prompt)
 
-    dataset_path = Path(config_file["dataset_path"])
+
+def prepare_dataset(path: Path) -> tuple[list[Path], dict | None]:
     pdf_paths = []
-    for file in dataset_path.iterdir():
+    expected_output = None
+    for file in path.iterdir():
         if file.suffix == ".pdf":
             pdf_paths.append(Path(file))
+        elif file.name == "expected_output.json":
+            with open(file, mode="r", encoding="utf-8") as f:
+                expected_output = json.load(f)
+    return (pdf_paths, expected_output)
+
+
+def run_conversions(paths: list[Path]) -> dict:
     print("Converting...")
     conversion_result = {}
     with ThreadPoolExecutor() as executor:
-        conversion_tasks = {executor.submit(convert, file_path): file_path.name for file_path in pdf_paths}
+        conversion_tasks = {
+            executor.submit(convert, file_path): file_path.name for file_path in paths
+        }
         for future in concurrent.futures.as_completed(conversion_tasks):
             converted_file = conversion_tasks[future]
             try:
                 conversion_result[converted_file] = future.result()
             except Exception as exc:
                 print(f"{converted_file} caused an exception: {exc}")
+    return conversion_result
 
+
+async def run_extractions(
+    output_schema_path: Path,
+    config: ModelConfig,
+    system_prompt: str,
+    user_prompt: str,
+    conversion_result: dict,
+) -> dict:
     print("Extracting...")
-    with open("./output_schema.json", "r") as file:
+    with open(output_schema_path, "r") as file:
         output_schema = json.load(file)
- 
+
     extract_func = None
 
     if config.provider == Provider.OLLAMA:
         extract_func = extract_with_ollama
     else:
-        extract_func = extract_with_openai  
+        extract_func = extract_with_openai
 
     params = {
-            "config": config,
-            "system_prompt": system_prompt,
-            "user_prompt": user_prompt,
-            "output_schema": output_schema
-            }
+        "config": config,
+        "system_prompt": system_prompt,
+        "user_prompt": user_prompt,
+        "output_schema": output_schema,
+    }
 
     assert extract_func is not None
     extraction_tasks: dict[str, asyncio.Task] = {
@@ -80,13 +103,59 @@ async def extract():
     }
 
     await asyncio.gather(*extraction_tasks.values())
-    extraction_results = {
-        file: task.result()
-        for file, task in extraction_tasks.items()
-    }
+    extraction_results = {}
 
-    print(extraction_results)
-    return(extraction_results)
-    
+    for file, task in extraction_tasks.items():
+        try:
+            result = task.result()
+            extraction_results[file] = json.loads(result)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON for {file}: {e}")
+            extraction_results[file] = {"error": "Invalid JSON response"}
+        except Exception as e:
+            print(f"An error occurred during extraction for {file}: {e}")
+            extraction_results[file] = {"error": str(e)}
+
+    return extraction_results
+
+
+async def run_pipeline(config_path: Path, output_schema_path: Path, dataset_path: Path):
+    config, system_prompt, user_prompt = load_config(path=config_path)
+    pdf_paths, expected_output = prepare_dataset(dataset_path)
+
+    conversion_result = run_conversions(paths=pdf_paths)
+    extraction_results = await run_extractions(
+        output_schema_path=output_schema_path,
+        config=config,
+        system_prompt=system_prompt,
+        user_prompt=user_prompt,
+        conversion_result=conversion_result,
+    )
+
+    return (extraction_results, expected_output)
+
+
+async def main():
+    args = parser.parse_args()
+    config_path = Path(args.config_path)
+    output_schema_path = Path(args.output_schema_path)
+    dataset_path = Path(args.dataset_path)
+
+    if not config_path.exists():
+        raise FileNotFoundError(f"Config File not found: {config_path}")
+    if not output_schema_path.exists():
+        raise FileNotFoundError(f"Output Schema JSON not found: {output_schema_path}")
+    if not dataset_path.exists():
+        raise FileNotFoundError(f"Dataset not found: {dataset_path}")
+
+    result = await run_pipeline(
+        config_path=config_path,
+        output_schema_path=output_schema_path,
+        dataset_path=dataset_path,
+    )
+
+    print(result)
+
+
 if __name__ == "__main__":
-    asyncio.run(extract())
+    asyncio.run(main())
