952d7720cb8d559f195b1d3924f56a590e2b2e57
Marvin Minuth <marvin.minuth@gmail.com>
2025-07-09 06:33:27 +0200

    Refactor prepare_dataset and run_pipeline functions to streamline expected output handling

--- src/datex/main.py
+++ src/datex/main.py
@@ -19,22 +19,26 @@ def load_config(path: Path) -> ExtractionConfig:
     return config
 
 
-def prepare_dataset(path: Path) -> tuple[list[Path], dict | None]:
+def prepare_dataset(path: Path) -> list[Path]:
     pdf_paths = []
-    expected_output = None
     for file in path.iterdir():
         if file.suffix == ".pdf":
             pdf_paths.append(file)
-        elif file.name == "expected_output.json":
-            with open(file, mode="r", encoding="utf-8") as f:
-                expected_output = json.load(f)
-    return (pdf_paths, expected_output)
+    return pdf_paths
 
 
-async def run_pipeline(config_path: Path, output_schema_path: Path, dataset_path: Path):
+async def run_pipeline(
+    config_path: Path,
+    output_schema_path: Path,
+    dataset_path: Path,
+    expected_result_path: Path,
+):
     config = load_config(path=config_path)
-    pdf_paths, expected_output = prepare_dataset(dataset_path)
 
+    with open(expected_result_path, "r") as file:
+        expected_result = json.load(file)
+
+    pdf_paths = prepare_dataset(path=dataset_path)
     conversion_result = run_conversions(paths=pdf_paths)
     extraction_results = await run_extractions(
         output_schema_path=output_schema_path,
@@ -42,31 +46,38 @@ async def run_pipeline(config_path: Path, output_schema_path: Path, dataset_path
         conversion_result=conversion_result,
     )
 
-    return (extraction_results, expected_output)
+    return (extraction_results, expected_result)
+
+
+def dir_path(str):
+    path = Path(str)
+    if not path.exists():
+        raise NotADirectoryError(f"Directory not found: {path}")
+    else:
+        return path
+
+
+def file_path(str):
+    path = Path(str)
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {path}")
+    else:
+        return path
 
 
 async def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_path")
-    parser.add_argument("output_schema_path")
-    parser.add_argument("dataset_path")
+    parser.add_argument("config_path", type=file_path)
+    parser.add_argument("output_schema_path", type=file_path)
+    parser.add_argument("dataset_path", type=dir_path)
+    parser.add_argument("expected_result_path", type=file_path)
 
     args = parser.parse_args()
-    config_path = Path(args.config_path)
-    output_schema_path = Path(args.output_schema_path)
-    dataset_path = Path(args.dataset_path)
-
-    if not config_path.exists():
-        raise FileNotFoundError(f"Config File not found: {config_path}")
-    if not output_schema_path.exists():
-        raise FileNotFoundError(f"Output Schema JSON not found: {output_schema_path}")
-    if not dataset_path.exists():
-        raise FileNotFoundError(f"Dataset not found: {dataset_path}")
-
     result = await run_pipeline(
-        config_path=config_path,
-        output_schema_path=output_schema_path,
-        dataset_path=dataset_path,
+        config_path=args.config_path,
+        output_schema_path=args.output_schema_path,
+        dataset_path=args.dataset_path,
+        expected_result_path=args.expected_result_path,
     )
 
     print(result)
