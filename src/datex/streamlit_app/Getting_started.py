2345d924115eb0f5cfe32b3cfbfeb4e434b21b44
Marvin Minuth <marvin.minuth@gmail.com>
2025-07-10 19:29:15 +0200

    Reorganized pages

--- src/datex/streamlit_app/Getting_started.py
+++ src/datex/streamlit_app/Getting_started.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Getting started",
-    page_icon="🚀"
-)
+st.set_page_config(page_title="Getting started", page_icon="🚀", layout="wide")
 
 st.write("# Welcome to Datex!")
 
